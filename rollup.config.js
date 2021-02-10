5ea96825dd500db78bbe24dec97092949bde2382
mguomanila <12101668+mguomanila@users.noreply.github.com>
2021-02-10 18:05:55 +0800

    fix and update package

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
@@ -10,52 +11,44 @@ import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
-export default [
-  {
-    input: 'src/lib/index.js',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      postcss({
-        plugins: [],
-        minimize: true,
-        sourceMap: 'inline',
-      }),
-      external({
-        includeDependencies: true,
-      }),
-      url(),
-      svgr(),
-      resolve(),
-      babel({
-        presets: ['react-app'],
-        plugins: [
-          '@babel/plugin-proposal-object-rest-spread',
-          '@babel/plugin-proposal-optional-chaining',
-          '@babel/plugin-syntax-dynamic-import',
-          '@babel/plugin-proposal-class-properties',
-          'transform-react-remove-prop-types',
-        ],
-        exclude: 'node_modules/**',
-        runtimeHelpers: true,
-      }),
-      commonjs(),
-      terser(),
-    ],
-  },
-  {
-    input: 'src/lib/index.d.ts',
-    output: [{ file: pkg.typings, format: 'es' }],
-    plugins: [dts()],
-  },
-]
+export default [{
+  input: 'src/lib/index.js',
+  output: [ {
+    file: pkg.main,
+    format: 'cjs',
+    sourcemap: true,
+  }, {
+    file: pkg.module,
+    format: 'es',
+    sourcemap: true,
+  }, ],
+  plugins: [
+    postcss({
+      plugins: [],
+      minimize: true,
+      sourceMap: 'inline',
+    }),
+    external({ includeDependencies: true, }),
+    url(),
+    svgr(),
+    resolve(),
+    babel({
+      presets: ['react-app'],
+      plugins: [
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+        'transform-react-remove-prop-types',
+      ],
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+    }),
+    commonjs(),
+    terser(),
+  ],
+}, {
+  input: 'src/lib/index.d.ts',
+  output: [{ file: pkg.typings, format: 'es' }],
+  plugins: [dts()],
+}, ]
